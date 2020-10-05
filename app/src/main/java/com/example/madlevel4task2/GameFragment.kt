81d53685b153737f72556db6ee875f9da42f014b
Kai <Kaivandenbroek@hotmail.com>
2020-10-05 15:49:26 +0200

    finished the menubar en nav

--- app/src/main/java/com/example/madlevel4task2/GameFragment.kt
+++ app/src/main/java/com/example/madlevel4task2/GameFragment.kt
@@ -1,12 +1,12 @@
 package com.example.madlevel4task2
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import kotlin.random.Random
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -23,6 +23,25 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_main, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_history -> {
+                findNavController().navigate(R.id.fragment_history)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun randomOutcome() {
+        val random = Random
     }
 }
--- app/src/main/java/com/example/madlevel4task2/HistoryFragment.kt
+++ app/src/main/java/com/example/madlevel4task2/HistoryFragment.kt
@@ -1,10 +1,8 @@
 package com.example.madlevel4task2
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
 
@@ -18,14 +16,31 @@ class HistoryFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        activity?.setTitle(R.string.history_title)
         return inflater.inflate(R.layout.fragment_history, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        setHasOptionsMenu(true)
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_history, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+   // override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // TODO delete history
+//        return when(item.itemId) {
+//            R.id.action_history -> {
+//                findNavController().navigate(R.id.fragment_history)
+//                return true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+    //}
 }
